3a12cf4eef98a7538f9f96a2754bcceb52712bb9
Sunaina Yadav <sunainadev006@gmail.com>
2024-11-27 21:20:15 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -43,24 +43,7 @@ with col1:
              labels={'center': 'Center', 'Issued_Patents': 'Number of Patents Issued'},
              color_discrete_sequence=["Coral"])
 # Display the chart in Streamlit
-  fig.update_layout(title=dict(
-        text='Total Patents Issued by each Center',  # Title text
-        font=dict(size=25, color='Coral')  # Set font size and color
-    ),
-    height=460,  
-    width=500, 
-    margin=dict(l=20, r=20, t=30, b=20),
-    font=dict(size=25),
-     yaxis=dict(
-        title=dict(font=dict(size=18)),  
-        tickfont=dict(size=18)           
-    ),
-     xaxis=dict(
-        title=dict(font=dict(size=18)), 
-        tickfont=dict(size=18)          
-    )
-) 
- 
+  
   st.plotly_chart(fig, use_container_width=True)
 with col2:
   df_status = pd.read_csv('output1.csv')  
